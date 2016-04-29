dd2c6e9e713f7fb4f2ac6780390bc4112c31951b
valentinbourgoin <valentin.bourgoin@gmail.com>
2016-04-29 11:25:03 +0200

    Clean gulpfile

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// var path = require('path');
-
-// require('fs')
-//   .readdirSync('./gulp/tasks/')
-//   .forEach(function(task) {
-//     if (path.extname(task) !== '.js') return;
-//     require('./gulp/tasks/' + task);
-//   });
-
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   watch = require('gulp-watch'),
